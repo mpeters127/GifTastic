73192e8c5a523a82c3e326cc5f21c473c7705619
m.peters <snakeyes721@outlook.com>
2019-08-28 21:20:30 -0700

    gifs are appearing. need to limit it to one button at a time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,6 @@ let states = ["california", "oregon", "washington"]
 $(document).ready(function () {
   makeButtons();
 
-  let endPoint = "https://api.giphy.com/v1/gifs/search";
-  let api_key = "Lt4Swoe1WVvDDtxMEjI3L6XmGXfJJ6Ny";
 
   // this function makes our buttons
   function makeButtons() {
@@ -22,18 +20,34 @@ $(document).ready(function () {
     stateButton.text(text);
 
     $("#stateButtons").append(stateButton);
+    getGifsForButton(text);
   }
 
   function getGifsForButton(text) {
-    let queryURL = endPoint + "?q=" + text + "&api_key=" + api_key;
-    
-    
+    let endPoint = "https://api.giphy.com/v1/gifs/search";
+    let api_key = "Lt4Swoe1WVvDDtxMEjI3L6XmGXfJJ6Ny";
+    let queryURL = endPoint + "?q=" + text + "&limit=10" + "&api_key=" + api_key;
+
+
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function (response) {
+      
+      for (let i = 0; i < response.data.length; i++){
+
+        let gifDiv = $("<div>");
+        
+        let stateGif = $("<img>");
+        stateGif.attr("src", response.data[i].images.fixed_height.url);
+  
+        //gifDiv.append(p);
+        gifDiv.append(stateGif);
+        
+        $("#stateGifs").prepend(gifDiv);
+      }
       console.log(response);
-    }).catch(function(error){
+    }).catch(function (error) {
       console.error(error)
     });
   }

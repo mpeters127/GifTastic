74b8a3277e47d08b43c0c9ab2dc9f93225cdab53
m.peters <snakeyes721@outlook.com>
2019-08-28 21:03:49 -0700

    buttons are going into array. working on api populating gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,74 +1,46 @@
-let states = ["california","oregon","washington"]
+let states = ["california", "oregon", "washington"]
+// MJP <3 CMG
+$(document).ready(function () {
+  makeButtons();
 
-// this function makes our buttons
+  let endPoint = "https://api.giphy.com/v1/gifs/search";
+  let api_key = "Lt4Swoe1WVvDDtxMEjI3L6XmGXfJJ6Ny";
 
+  // this function makes our buttons
+  function makeButtons() {
+    //
+    $("#stateButtons").empty();
 
-
-let state = $(this).attr("data-state");
-// this function makes our JSON call to get our gifs
-
-let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + state + 
-  "&api_key=Lt4Swoe1WVvDDtxMEjI3L6XmGXfJJ6Ny";
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    // After data comes back from the request
-    .then(function(response) {
-      console.log(queryURL);
-
-      console.log(response);
-      // storing the data from the AJAX request in the results variable
-      let results = response.data;
-
-      // Looping through each result item
-      for (let i = 0; i < results.length; i++) {
-
-        // Creating and storing a div tag
-        let stateDiv = $("<div>");
-
-        // Creating a paragraph tag with the result item's rating
-        let p = $("<p>").text("Rating: " + results[i].rating);
-
-        // Creating and storing an image tag
-        let stateImage = $("<img>");
-        // Setting the src attribute of the image to a property pulled off the result item
-        stateImage.attr("src", results[i].images.fixed_height.url);
-
-        // Appending the paragraph and image tag to the stateDiv
-        stateDiv.append(p);
-        stateDiv.append(stateImage);
-
-        // Prependng the stateDiv to the HTML page in the "#gifs-appear-here" div
-        $("#stateGifs").append(stateButtons);
-      }
+    for (let i = 0; i < states.length; i++) {
+      makeOneButton(states[i]);
     }
+  }
+
+  function makeOneButton(text) {
+    let stateButton = $('<button></button>');
+    stateButton.addClass("state");
+    stateButton.text(text);
+
+    $("#stateButtons").append(stateButton);
+  }
+
+  function getGifsForButton(text) {
+    let queryURL = endPoint + "?q=" + text + "&api_key=" + api_key;
     
-    );
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    }).catch(function(error){
+      console.error(error)
+    });
+  }
 
-    function makeButtons() {
+  $('#newStateButton').click(function (event) {
+    event.preventDefault();
 
-      $("stateButtons").empty();
-  
-      for (let i = 0; i < states.length; i++){
-        
-        let stateButton = $('<button>');
-          stateButton.addClass("state");
-          stateButton.text(states[i])
-  
-        $("#stateButtons").append(stateButton);
-      }
-    }
-  
-    makeButtons();
-
-    $('#statesFrom').submit(function (event){
-      event.preventDefault();
-
-      //array for state selection
-      let stateSelection=[];
-      stateSelection.push($('#stateInput').val().trim());
-
-      makeButtons(stateSelection);
-    })
+    makeOneButton($('#stateInput').val().trim());
+  })
+})
